7980846e56812ac0269eb4c7b88007e1a374c1c1
DELL <pujariparvati1022@gmail.com>
2026-01-09 10:57:43 +0100

    Added fixed Commit.

--- TestNGProject/src/test/java/Utilities/Genericfunctions.java
+++ TestNGProject/src/test/java/Utilities/Genericfunctions.java
@@ -6,11 +6,10 @@ import org.openqa.selenium.WebDriver;
 public class Genericfunctions {
 	public void Click(WebDriver driver, By locator) {
 		driver.findElement(locator).click();
-
 	}
+
 	public void Sendkeys(WebDriver driver, By locator, String Value) {
 		driver.findElement(locator).sendKeys(Value);
-		
 	}
 
 }
--- TestNGProject/src/test/java/pages/RegPage.java
+++ TestNGProject/src/test/java/pages/RegPage.java
@@ -7,28 +7,21 @@ import Utilities.Genericfunctions;
 
 public class RegPage {
 	WebDriver driver;
-	
+
 	public RegPage(WebDriver driver) {
 		this.driver = driver;
-		
 	}
-	
+
 	By DoNotConsent = By.cssSelector("[aria-label='Do not consent']");
 	By FirstName = By.cssSelector("[placeholder='First Name']");
 	By LastName = By.cssSelector("[placeholder='Last Name']");
-	
-	
+
 	Genericfunctions ObjGenericfunctions = new Genericfunctions();
-	
+
 	public void EnterDetails() {
 		ObjGenericfunctions.Click(driver, DoNotConsent);
 		ObjGenericfunctions.Sendkeys(driver, FirstName, "Sham");
 		ObjGenericfunctions.Sendkeys(driver, LastName, "Naik");
-		
-		
-		
-	}
-	
-	
 
+	}
 }
